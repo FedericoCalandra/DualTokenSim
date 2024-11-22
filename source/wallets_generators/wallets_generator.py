1290389a83880df57a95fbb4481fe2e83239cdd3
Federico_Calandra <federico12597@gmail.com>
2024-11-22 19:43:05 +0100

    Added exponential_wallets_generator.py and related tests

--- source/wallets_generators/wallets_generator.py
+++ source/wallets_generators/wallets_generator.py
@@ -6,29 +6,30 @@ class WalletsGenerator(ABC):
     Abstract base class for generating wallets with random distributions of token supplies.
 
     Attributes:
-        total_free_token_supply (float): The total amount of tokens available for distribution
-                                         across all generated wallets.
+        probability_associated_to_total_free_token (float): Probability associated to the total amount of tokens
+                                                            available for distribution.
     Methods:
         get_random_wallet: Abstract method to generate a wallet with a specific allocation of tokens.
     """
 
-    def __init__(self, total_free_token_supply: float):
+    def __init__(self, probability_associated_to_total_free_token: float):
         """
         Initializes the WalletsGenerator with a total supply of tokens available for distribution.
 
         Args:
-            total_free_token_supply (float): Total amount of tokens to be distributed. Must be positive.
+            probability_associated_to_total_free_token (float): Probability associated to total amount of tokens to be
+                                                                distributed. Must be positive.
 
         Raises:
             ValueError: If total_free_token_supply is not positive.
         """
-        if total_free_token_supply < 0:
-            raise ValueError("Total free token supply must be a non-negative value.")
+        if probability_associated_to_total_free_token < 0:
+            raise ValueError("Probability associated to total free token supply must be a non-negative value.")
 
-        self.total_free_token_supply = total_free_token_supply
+        self.probability_associated_to_total_free_token = probability_associated_to_total_free_token
 
     @abstractmethod
-    def get_random_wallet(self):
+    def get_random_wallet(self, total_free_token_supply):
         """
         Abstract method to generate a random wallet with a specific allocation of tokens.
 
